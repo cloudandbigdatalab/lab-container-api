8f2fd8dee5f75908d505b43a76ecc5d955aa8d00
shawnaten <shawnmaten@gmail.com>
2015-09-25 18:18:26 -0500

    working on error when host already created

--- draft.py
+++ draft.py
@@ -1,9 +1,18 @@
-import subprocess
+from subprocess import check_output, CalledProcessError, STDOUT
 
 # only Digital Ocean for now
 def connect(token):
   f = open("debug.txt", "w")
-  subprocess.check_call(["docker-machine", "create", "-d", "digitalocean", "--digitalocean-access-token="+token, "lab-container-api"], stdout=f, stderr=subprocess.STDOUT)
+
+  machine_output = None
+  try:
+      machine_output = check_output([
+      "docker-machine", "create", "-d", "digitalocean",
+      "--digitalocean-access-token="+token, "lab-container-api"
+      ], stderr=STDOUT)
+  except CalledProcessError:
+      print machine_output
+
   f.close()
 
 connect("f8edd967671537a7d77ed65ac992296b89940032df26aa1345ce9d23b68fcf84")
