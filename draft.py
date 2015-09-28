5519b96338f71a4cd613ca2f546ed8e2ece4be99
shawnaten <shawnmaten@gmail.com>
2015-09-28 16:32:09 -0500

    Change handling of preexisting machine case
    The error of a preexisting machine is properly handled. The output of
    successful and unsuccessful create attempts print properly.

--- draft.py
+++ draft.py
@@ -1,18 +1,18 @@
 from subprocess import check_output, CalledProcessError, STDOUT
+import os
 
-# only Digital Ocean for now
 def connect(token):
-  f = open("debug.txt", "w")
-
-  machine_output = None
   try:
-      machine_output = check_output([
-      "docker-machine", "create", "-d", "digitalocean",
-      "--digitalocean-access-token="+token, "lab-container-api"
+      output = check_output([
+      "docker-machine",
+      "create",
+      "-d",
+      "digitalocean",
+      "--digitalocean-access-token="+token,
+      "lab-container-api"
       ], stderr=STDOUT)
-  except CalledProcessError:
-      print machine_output
+      print output
+  except CalledProcessError as error:
+      print error.output
 
-  f.close()
-
-connect("f8edd967671537a7d77ed65ac992296b89940032df26aa1345ce9d23b68fcf84")
+connect(os.environ['DO_KEY'])
